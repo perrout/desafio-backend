022d278983f34dab8b1c59dabef8a88851c9b743
Marcus Perrout <contato@perrout.com.br>
2021-09-20 00:23:23 -0300

    refactor: authorization service

--- app/app/Services/Authorization/AuthorizationService.php
+++ app/app/Services/Authorization/AuthorizationService.php
@@ -8,17 +8,18 @@ use Illuminate\Support\Facades\Http;
 class AuthorizationService implements AuthorizationServiceContract
 {
     private $httpClient;
+    private $endpoint;
 
     public function __construct(Http $httpClient)
     {
         $this->httpClient = $httpClient;
+        $this->endpoint = env('AUTHORIZATION_ENDPOINT', 'https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6');
     }
 
     public function status()
     {
         try {
-            $url = env('AUTHORIZATION_URL', 'https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6');
-            $response = $this->httpClient::get($url);
+            $response = $this->httpClient::get($this->endpoint);
             if ($response->ok() && $response->json('message') === 'Autorizado') {
                 return true;
             }
